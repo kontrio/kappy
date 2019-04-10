8921e1da03e9b9501e40597ae1e0085466d645ac
Sam Giles <sam.e.giles@gmail.com>
2019-04-10 03:00:54 +0200

    Updating to use official client

--- pkg/kubernetes/deployment.go
+++ pkg/kubernetes/deployment.go
@@ -8,11 +8,12 @@ import (
 	"time"
 
 	"github.com/apex/log"
-	"github.com/ericchiang/k8s"
-	appsv1 "github.com/ericchiang/k8s/apis/apps/v1"
-	corev1 "github.com/ericchiang/k8s/apis/core/v1"
-	metav1 "github.com/ericchiang/k8s/apis/meta/v1"
 	"github.com/kontrio/kappy/pkg/model"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 )
 
 var _true bool = true
@@ -43,19 +44,19 @@ func createDeploymentResource(serviceDef *model.ServiceDefinition, serviceConfig
 		namespace = "default"
 	}
 
-	containers := []*corev1.Container{}
+	containers := []corev1.Container{}
 
 	for _, container := range serviceDef.Containers {
 		containerName := container.Name
 		containerConfig := serviceConfig.GetContainerConfigByName(containerName)
 
-		envVars := []*corev1.EnvVar{}
+		envVars := []corev1.EnvVar{}
 
 		if containerConfig != nil {
 			for key, value := range containerConfig.Env {
-				envVars = append(envVars, &corev1.EnvVar{
-					Name:  &key,
-					Value: &value,
+				envVars = append(envVars, corev1.EnvVar{
+					Name:  key,
+					Value: value,
 				})
 			}
 		}
@@ -63,27 +64,27 @@ func createDeploymentResource(serviceDef *model.ServiceDefinition, serviceConfig
 		log.Debugf("Configuring container: %s.%s", serviceName, containerName)
 
 		defaultContainerPort := corev1.ContainerPort{
-			ContainerPort: &container.ExposePort,
+			ContainerPort: container.ExposePort,
 		}
 
 		imageName := canonicalImageName(container.Image, dockerRegistry, deployVersion, container.Build != nil)
 
 		secretName := fmt.Sprintf("%s-%s-secrets", serviceName, containerName)
 
-		containers = append(containers, &corev1.Container{
-			Name:    &container.Name,
-			Image:   &imageName,
+		containers = append(containers, corev1.Container{
+			Name:    container.Name,
+			Image:   imageName,
 			Command: container.Command,
 			Args:    container.Args,
-			Ports:   []*corev1.ContainerPort{&defaultContainerPort},
+			Ports:   []corev1.ContainerPort{defaultContainerPort},
 			Env:     envVars,
-			EnvFrom: []*corev1.EnvFromSource{
-				&corev1.EnvFromSource{
-					SecretRef: &corev1.SecretEnvSource{
-						LocalObjectReference: &corev1.LocalObjectReference{
-							Name: &secretName,
+			EnvFrom: []corev1.EnvFromSource{
+				corev1.EnvFromSource{
+					SecretRef: corev1.SecretEnvSource{
+						LocalObjectReference: corev1.LocalObjectReference{
+							Name: secretName,
 						},
-						Optional: &_true,
+						Optional: true,
 					},
 				},
 			},
@@ -91,149 +92,59 @@ func createDeploymentResource(serviceDef *model.ServiceDefinition, serviceConfig
 	}
 
 	deploymentSpec := appsv1.DeploymentSpec{
-		Replicas: &serviceDef.Replicas,
-		Selector: &metav1.LabelSelector{
+		Replicas: serviceDef.Replicas,
+		Selector: metav1.LabelSelector{
 			MatchLabels: map[string]string{
 				"name": serviceDef.Name,
 			},
 		},
-		Template: &corev1.PodTemplateSpec{
-			Metadata: &metav1.ObjectMeta{
-				Name:      &serviceDef.Name,
-				Namespace: &namespace,
+		Template: corev1.PodTemplateSpec{
+			Metadata: metav1.ObjectMeta{
+				Name:      serviceDef.Name,
+				Namespace: namespace,
 				Labels: map[string]string{
 					"name":          serviceDef.Name,
 					"kappy.managed": serviceDef.Name,
 				},
 			},
-			Spec: &corev1.PodSpec{
+			Spec: corev1.PodSpec{
 				Containers: containers,
 			},
 		},
 	}
 
 	deployment := appsv1.Deployment{
-		Metadata: &metav1.ObjectMeta{
-			Name:      &serviceDef.Name,
-			Namespace: &namespace,
+		Metadata: metav1.ObjectMeta{
+			Name:      serviceDef.Name,
+			Namespace: namespace,
 			Labels: map[string]string{
 				"name":          serviceDef.Name,
 				"kappy.managed": serviceDef.Name,
 			},
 		},
-		Spec: &deploymentSpec,
+		Spec: deploymentSpec,
 	}
 
 	return deployment
 }
 
-func CreateUpdateDeployment(client *k8s.Client, serviceDefinition *model.ServiceDefinition, serviceConfig *model.ServiceConfig, namespace, deployVersion, dockerRegistry string) error {
+func UpsertDeployment(client *kubernetes.ClientSet, serviceDefinition *model.ServiceDefinition, serviceConfig *model.ServiceConfig, namespace, deployVersion, dockerRegistry string) error {
 	deployment := createDeploymentResource(serviceDefinition, serviceConfig, namespace, deployVersion, dockerRegistry)
-	err := client.Create(context.Background(), &deployment)
 
-	if err != nil {
-		if apiErr, ok := err.(*k8s.APIError); ok {
-			if apiErr.Code == http.StatusConflict {
-				err = client.Update(context.Background(), &deployment)
-				if err != nil {
-					return err
-				}
-
+	upsert := UpsertCommand{
+		Create: func() (err error) {
+			_, err = client.AppsV1().Deployments(namespace).Create(&deployment)
+			if err == nil {
+				log.Infof("Created deployment %s", serviceDefinition.Name)
+			}
+		},
+		Update: func() (err error) {
+			_, err = client.AppsV1().Deployments(namespace).Update(&deployment)
+			if err == nil {
 				log.Infof("Updated deployment %s", serviceDefinition.Name)
 			}
-		}
-		return err
+		},
 	}
 
-	log.Infof("Created deployment %s", serviceDefinition.Name)
-	return nil
-}
-
-func WatchDeploymentRollout(client *k8s.Client, serviceName, namespace string) error {
-	deployment := appsv1.Deployment{}
-
-	labelSelector := new(k8s.LabelSelector)
-	labelSelector.Eq("name", serviceName)
-
-	ctx, cancel := context.WithTimeout(context.Background(), 120*time.Second)
-	defer cancel()
-
-	watcher, err := client.Watch(ctx, namespace, &deployment, labelSelector.Selector())
-	if err != nil {
-		return err
-	}
-
-	defer watcher.Close()
-
-	for {
-		deployment := new(appsv1.Deployment)
-		eventType, errWatch := watcher.Next(deployment)
-
-		log.Debugf("Watched: %s", eventType)
-
-		if errWatch != nil {
-			return errWatch
-		}
-
-		// Check failures to enter debug mode
-		status := deployment.GetStatus()
-		condition := getDeploymentCondition(status, _deploymentReplicaFailure)
-
-		if condition != nil {
-			log.Errorf("replica failure: %s", condition.GetMessage())
-		}
-
-		message, ok, errStatus := getRolloutStatus(deployment)
-
-		log.Infof(message)
-
-		if errStatus != nil {
-			return errStatus
-		}
-
-		if ok {
-			return nil
-		}
-	}
-	return nil
-}
-
-var _deploymentProgressing string = "Progressing"
-var _deploymentReplicaFailure string = "ReplicaFailure"
-var _deploymentAvailable string = "Available"
-
-var _timedoutReason string = "ProgressDeadlineExceeded"
-
-func getRolloutStatus(deployment *appsv1.Deployment) (string, bool, error) {
-	status := deployment.GetStatus()
-	condition := getDeploymentCondition(status, _deploymentProgressing)
-	name := *deployment.Metadata.Name
-
-	if condition != nil && condition.GetReason() == _timedoutReason {
-		return "", false, fmt.Errorf("deployment %q exceeded its progress deadline", name)
-	}
-
-	if status.GetUpdatedReplicas() < deployment.GetSpec().GetReplicas() {
-		return fmt.Sprintf("Waiting for deployment %q rollout to finish: %d of %d new replicas have been updated...", name, status.GetUpdatedReplicas(), deployment.GetSpec().GetReplicas()), false, nil
-	}
-
-	if status.GetReplicas() > status.GetUpdatedReplicas() {
-		return fmt.Sprintf("Waitinf for deployment %q rollout to finish: waiting for %d old replicas to terminate...", name, status.GetReplicas()-status.GetUpdatedReplicas()), false, nil
-	}
-
-	if status.GetAvailableReplicas() < status.GetUpdatedReplicas() {
-		return fmt.Sprintf("Waiting for deployment %q rollout to finish: %d of %d updated replicas are available...", name, status.GetAvailableReplicas(), status.GetUpdatedReplicas()), false, nil
-	}
-
-	return fmt.Sprintf("deployment %q successfully rolled out", name), true, nil
-}
-
-func getDeploymentCondition(deploymentStatus *appsv1.DeploymentStatus, conditionStatus string) *appsv1.DeploymentCondition {
-	for _, condition := range deploymentStatus.GetConditions() {
-		log.Debugf("Condition type: if %s == %s", *condition.Type, conditionStatus)
-		if *condition.Type == conditionStatus {
-			return condition
-		}
-	}
-	return nil
+	return upsert.Do()
 }
--- pkg/kubernetes/service.go
+++ pkg/kubernetes/service.go
@@ -4,11 +4,11 @@ import (
 	"context"
 	"net/http"
 
-	"github.com/ericchiang/k8s"
-	corev1 "github.com/ericchiang/k8s/apis/core/v1"
-	metav1 "github.com/ericchiang/k8s/apis/meta/v1"
-	"github.com/ericchiang/k8s/util/intstr"
 	"github.com/kontrio/kappy/pkg/model"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/client-go/kubernetes"
 )
 
 func createService(serviceDefinition *model.ServiceDefinition, namespace string) corev1.Service {
@@ -20,36 +20,41 @@ func createService(serviceDefinition *model.ServiceDefinition, namespace string)
 	var targetPort int32 = 3000
 
 	return corev1.Service{
-		Metadata: &metav1.ObjectMeta{
-			Name:      &serviceDefinition.Name,
-			Namespace: &namespace,
+		Metadata: metav1.ObjectMeta{
+			Name:      serviceDefinition.Name,
+			Namespace: namespace,
 		},
-		Spec: &corev1.ServiceSpec{
+		Spec: corev1.ServiceSpec{
 			Selector: serviceSelector,
-			Ports: []*corev1.ServicePort{
-				&corev1.ServicePort{
-					Port: &port,
-					TargetPort: &intstr.IntOrString{
-						IntVal: &targetPort,
-					},
+			Ports: []corev1.ServicePort{
+				corev1.ServicePort{
+					Port:       port,
+					TargetPort: intstr.FromInt(targetPort),
 				},
 			},
 		},
 	}
 }
 
-func CreateUpdateService(client *k8s.Client, serviceDefinition *model.ServiceDefinition, namespace string) error {
-	resource := createService(serviceDefinition, namespace)
-	err := client.Create(context.Background(), &resource)
-	if err != nil {
-		if apiErr, ok := err.(*k8s.APIError); ok {
-			if apiErr.Code == http.StatusConflict {
-				return nil
-			} else {
-				return err
+func CreateUpdateService(client *kubernetes.ClientSet, serviceDefinition *model.ServiceDefinition, namespace string) error {
+	service := createService(serviceDefinition, namespace)
+
+	upsertCmd := UpsertCommand{
+		Create: func() (err error) {
+			_, err = client.CoreV1().Services(namespace).Create(&service)
+
+			if err == nil {
+				log.Infof("Created service %s", serviceDefinition.Name)
 			}
-		}
+		},
+		Update: func() (err error) {
+			_, err = client.CoreV1().Services(namespace).Update(&service)
+
+			if err == nil {
+				log.Infof("Updated service %s", serviceDefinition.Name)
+			}
+		},
 	}
 
-	return nil
+	return upsertCmd.Do()
 }
