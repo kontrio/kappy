b9f93a70192ac06e4c351728dba3057b030269a7
Sam Giles <sam.e.giles@gmail.com>
2019-04-11 01:42:33 +0200

    Update secrets to new API

--- pkg/kubernetes/secrets.go
+++ pkg/kubernetes/secrets.go
@@ -5,46 +5,52 @@ import (
 	"net/http"
 
 	"github.com/apex/log"
-	"github.com/ericchiang/k8s"
 
-	corev1 "github.com/ericchiang/k8s/apis/core/v1"
-	metav1 "github.com/ericchiang/k8s/apis/meta/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 )
 
-func CreateSecret(client *k8s.Client, secretRef, namespace string, secrets map[string]string, labels map[string]string) error {
-	// Only alias to default for logging purposes, sending the empty string in the protobuf
-	// defaults to default anyway and is more efficient
+func CreateSecret(client *kubernetes.ClientSet, secretRef, namespace string, secrets map[string]string, labels map[string]string) error {
+	// Alias to default only for logging purposes.
 	ns := namespace
 	if len(namespace) == 0 {
-		ns = "default"
+		ns = corev1.NamespaceDefault
 	}
 
 	log.Infof("Creating secrets '%s' in namespace: '%s'", secretRef, ns)
 
+	encodedSecrets := base64EncodeMapOfStrings(secrets)
+
 	secret := corev1.Secret{
-		Metadata: &metav1.ObjectMeta{
-			Name:      &secretRef,
-			Namespace: &namespace,
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      secretRef,
+			Namespace: namespace,
 			Labels:    labels,
 		},
-		StringData: secrets,
+		Data: encodedSecrets,
 	}
 
-	return CreateUpdateK8sResource(client, context.Background(), &secret)
+	return doCreateSecret(client, secretRef, namespace, secret)
 }
 
-func CreateUpdateK8sResource(client *k8s.Client, ctx context.Context, resource *corev1.Secret) error {
-	err := client.Create(ctx, resource)
-	if err != nil {
-		if apiErr, ok := err.(*k8s.APIError); ok {
-			if apiErr.Code == http.StatusConflict {
-				err = client.Update(ctx, resource)
-				if err != nil {
-					return err
-				}
+func doCreateSecret(client *kubertnetes.ClientSet, secretRef, namespace string, resource corev1.Secret) error {
+	upsertCmd := UpsertCommand{
+		Create: func() (err error) {
+			err = client.CoreV1().Secrets(namespace).Create(&resource)
+
+			if err == nil {
+				log.Infof("Successfully created secret %s in namespace %s", secretRef, namespace)
 			}
-		}
-		return err
+		},
+		Update: func() (err error) {
+			err = client.CoreV1().Secrets(namespace).Update(&resource)
+
+			if err == nil {
+				log.Infof("Successfully updated secret %s in namespace %s", secretRef, namespace)
+			}
+		},
 	}
-	return nil
+
+	return upsertCmd.Do()
 }
