32f11252c4a458bc217c6067295bf30398d77234
Sam Giles <sam.e.giles@gmail.com>
2019-04-19 18:17:22 +0200

    Add two smoke tests

--- hack/integration-tests.sh
+++ hack/integration-tests.sh
@@ -1,3 +1,52 @@
 #!/usr/bin/env bash
 
+set -e
+
+machine=$(echo "$(uname -s)" | tr '[:upper:]' '[:lower:]')
+arch=$(echo "$(uname -m)" | sed s/^x86_64/amd64/g)
+
+TARGET=${machine}_${arch}
+KAPPY_BIN_DIR=./dist/$TARGET/
+
+kubectl proxy 2>&1 > /dev/stderr &
+
 kubectl get pods --all-namespaces
+
+
+kappy_deploy() {
+  set -e
+  $KAPPY_BIN_DIR/kappy version
+  $KAPPY_BIN_DIR/kappy deploy $1 --file ./test/kappy.test.$1.yaml --version 1
+}
+
+test_internalservice() {
+  kappy_deploy internalservice
+
+  result=$(curl -s http://127.0.0.1:8001/api/v1/namespaces/internalservice/services/http:echo-test:80/proxy/)
+
+  if [ "$result" != "\"received request\"" ];then 
+    echo "Request to internally deployed service failed"
+    return 1
+  fi
+
+  kubectl delete namespace/internalservice 
+  echo "PASSED"
+}
+
+test_externalservice() {
+  kappy_deploy externalservice
+
+  result=$(curl -s https://$(minikube ip)/ -H "Host: testecho.kappycitests.kontr.io" -k)
+
+  if [ "$result" != "\"received request\"" ];then 
+    echo "Request to externally deployed service failed"
+    return 1
+  fi
+  
+  kubectl delete namespace/externalservice 
+  echo "PASSED"
+}
+
+test_internalservice
+test_externalservice
+
